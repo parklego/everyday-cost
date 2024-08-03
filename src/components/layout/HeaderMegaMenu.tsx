ae44d1f94d50fd299f9522ffe9cfcc2c67587f63
parklego <hbd9425@gmail.com>
2024-08-04 04:16:52 +0900

    헤더 컴포넌트 추가

---
+++ src/components/layout/HeaderMegaMenu.tsx
@@ -0,0 +1,188 @@
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+
+import {
+  HoverCard,
+  Group,
+  Button,
+  UnstyledButton,
+  Text,
+  SimpleGrid,
+  ThemeIcon,
+  Anchor,
+  Divider,
+  Center,
+  Box,
+  Burger,
+  Drawer,
+  Collapse,
+  ScrollArea,
+  rem,
+  useMantineTheme,
+} from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
+import { IconChevronDown } from "@tabler/icons-react";
+import classes from "./HeaderMegaMenu.module.css";
+import { serviceList } from "@/data/list";
+
+export function HeaderMegaMenu() {
+  const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
+    useDisclosure(false);
+  const [linksOpened, { toggle: toggleLinks }] = useDisclosure(false);
+  const theme = useMantineTheme();
+
+  const links = serviceList.map((item) => (
+    <UnstyledButton className={classes.subLink} key={item.title}>
+      <Group wrap="nowrap" align="flex-start">
+        <ThemeIcon size={34} variant="default" radius="md">
+          <item.icon
+            style={{ width: rem(22), height: rem(22) }}
+            color={theme.colors.blue[6]}
+          />
+        </ThemeIcon>
+        <div>
+          <Text size="sm" fw={500}>
+            {item.title}
+          </Text>
+          <Text size="xs" c="dimmed">
+            {item.description}
+          </Text>
+        </div>
+      </Group>
+    </UnstyledButton>
+  ));
+
+  return (
+    <div className={classes.container}>
+      <header className={classes.header}>
+        <Group justify="space-between" h="100%">
+          {/* Todo : brand && logo */}
+          <p>Everyday cost</p>
+          <Group h="100%" gap={0} visibleFrom="sm">
+            <a href="#" className={classes.link}>
+              Home
+            </a>
+            <HoverCard
+              width={600}
+              position="bottom"
+              radius="md"
+              shadow="md"
+              withinPortal
+            >
+              <HoverCard.Target>
+                <a href="#" className={classes.link}>
+                  <Center inline>
+                    <Box component="span" mr={5}>
+                      Services
+                    </Box>
+                    <IconChevronDown
+                      style={{ width: rem(16), height: rem(16) }}
+                      color={theme.colors.blue[6]}
+                    />
+                  </Center>
+                </a>
+              </HoverCard.Target>
+
+              <HoverCard.Dropdown style={{ overflow: "hidden" }}>
+                <Group justify="space-between" px="md">
+                  <Text fw={500}>Services</Text>
+                </Group>
+
+                <Divider my="sm" />
+
+                <SimpleGrid cols={2} spacing={0}>
+                  {links}
+                </SimpleGrid>
+
+                <div className={classes.dropdownFooter}>
+                  <Group justify="space-between">
+                    <div>
+                      <Text fw={500} fz="sm">
+                        이 기능이 있으면 정말 멋지지 않을까요?
+                        {/* Wouldn't it be awesome if we had this feature? */}
+                      </Text>
+                      <Text size="xs" c="dimmed">
+                        여기서 가장 엉뚱한 기능을 요청해보세요!
+                        {/* "Unleash your wildest feature requests here!" */}
+                      </Text>
+                    </div>
+                    <Button variant="default">
+                      요청하러 가기
+                      {/* Make a request */}
+                    </Button>
+                  </Group>
+                </div>
+              </HoverCard.Dropdown>
+            </HoverCard>
+            <a href="#" className={classes.link}>
+              Statistics
+            </a>
+            <a href="#" className={classes.link}>
+              News & Updates
+            </a>
+            <a href="#" className={classes.link}>
+              Contact
+            </a>
+          </Group>
+
+          <Group visibleFrom="sm">
+            <Button variant="default">Log in</Button>
+            <Button>Sign up</Button>
+          </Group>
+
+          <Burger
+            opened={drawerOpened}
+            onClick={toggleDrawer}
+            hiddenFrom="sm"
+          />
+        </Group>
+      </header>
+
+      <Drawer
+        opened={drawerOpened}
+        onClose={closeDrawer}
+        size="100%"
+        padding="md"
+        title="Navigation"
+        hiddenFrom="sm"
+        zIndex={1000000}
+      >
+        <ScrollArea h={`calc(100vh - ${rem(80)})`} mx="-md">
+          <Divider my="sm" />
+
+          <a href="#" className={classes.link}>
+            Home
+          </a>
+          <UnstyledButton className={classes.link} onClick={toggleLinks}>
+            <Center inline>
+              <Box component="span" mr={5}>
+                Services
+              </Box>
+              <IconChevronDown
+                style={{ width: rem(16), height: rem(16) }}
+                color={theme.colors.blue[6]}
+              />
+            </Center>
+          </UnstyledButton>
+          <Collapse in={linksOpened}>{links}</Collapse>
+          <a href="#" className={classes.link}>
+            Statistics
+          </a>
+          <a href="#" className={classes.link}>
+            News & Updates
+          </a>
+          <a href="#" className={classes.link}>
+            Contact
+          </a>
+
+          <Divider my="sm" />
+
+          <Group justify="center" grow pb="xl" px="md">
+            <Button variant="default">Log in</Button>
+            <Button>Sign up</Button>
+          </Group>
+        </ScrollArea>
+      </Drawer>
+    </div>
+  );
+}
